8c33eaf1abc952251c752de3f34c5a5676eeac2a
Ethan Go <ethango@bu.edu>
2019-12-04 14:05:37 -0500

    added gitignore + base mongoose implementation

--- project/project/src/server.js
+++ project/project/src/server.js
@@ -1,5 +1,6 @@
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
+const mongoose = require('mongoose');
 var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
@@ -9,6 +10,39 @@ var client_id = '1ca6c4fa378a440881203b24132c769f'; // Your client id
 var client_secret = 'a9194f0771c34c8d885d64ae4c6b7c76'; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
+/**
+ * Function to connect to Mongoose (localhost)
+ * Added by: Ethan T Go
+ */
+mongoose.connect('mongodb://localhost/user')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err))
+/**
+ * The following is a schema for user, every time a user is added we add their data into mongoDB with the follwoing semantics
+ */
+const userSchema = new mongoose.Schema({
+      name: String,
+      email: String,
+      tags: [ String ],
+      date: {type: Date, default: Date.now}
+  });
+//Storing into the collection
+const User = mongoose.model('User', courseSchema);
+
+/**
+ * TODO: Replace each variable name with information gathered from  Spotify oAuth, We want to retrieve the user's name from oAuth instead of hardcoding it here
+ */
+async function createUser(){
+  const course = new Course({
+      name: 'Postgres Course',
+      author: 'Ethan',
+      tags: ['postgres', 'database'],
+      isPublished: true
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
 
 /**
  * Generates a random string containing numbers and letters
