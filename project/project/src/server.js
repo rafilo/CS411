708f6b250bcf7106b728597bd9e0d4379f3dbe5d
Ethan Go <ethango@bu.edu>
2019-12-04 22:25:13 -0500

    ad'

--- project/project/src/server.js
+++ project/project/src/server.js
@@ -1,8 +1,6 @@
 const express = require('express'); // Express web server framework
-const request = require('request'); // "Request" library
 const mongoose = require('mongoose');
 const cors = require('cors');
-const querystring = require('querystring');
 const cookieParser = require('cookie-parser');
 const home_page = require('./routes/home');
 const callback = require('./routes/callback');
@@ -10,9 +8,6 @@ const refresh_token = require('./routes/refresh_token');
 const app = express();
 
 console.log(home_page);
-var client_id = '1ca6c4fa378a440881203b24132c769f'; // Your client id
-var client_secret = 'a9194f0771c34c8d885d64ae4c6b7c76'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Function to connect to Mongoose (localhost)
@@ -33,20 +28,6 @@ const userSchema = new mongoose.Schema({
 //Storing into the collection
 const User = mongoose.model('User', userSchema);
 
-/**
- * TODO: Replace each variable name with information gathered from  Spotify oAuth, We want to retrieve the user's name from oAuth instead of hardcoding it here
- */
-async function createUser(){
-  const user = new User({
-      name: 'Postgres Course',
-      author: 'Ethan',
-      tags: ['postgres', 'database'],
-      isPublished: true
-  });
-
-  const result = await user.save();
-  console.log(result);
-}
 
 
 //These are the routers
