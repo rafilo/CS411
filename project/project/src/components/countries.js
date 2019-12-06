990f7289be668b1e3e9fa3b59f649198eddd3db5
Ethan Go <ethango@bu.edu>
2019-12-05 20:05:22 -0500

    added description object

--- project/project/src/components/countries.js
+++ project/project/src/components/countries.js
@@ -1,29 +1,6 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 
-const contacts = [
- {
-   "id": "karen",
-   "name": "Karen Isgrigg",
-   "handle": "karen_isgrigg",
-   "avatarURL": "http://localhost:5001/karen.jpg"
- },
- {
-   "id": "richard",
-   "name": "Richard Kalehoff",
-   "handle": "richardkalehoff",
-   "avatarURL": "http://localhost:5001/richard.jpg"
- },
- {
-   "id": "tyler",
-   "name": "Tyler McGinnis",
-   "handle": "tylermcginnis",
-   "avatarURL": "http://localhost:5001/tyler.jpg"
- }
-];
-
-
-
 class App extends Component {
   state = {
     country_name: "",
